9c744d19b21d76c74eb228a3ab6a57564446dc96
Rob Foulkrod <Azure.Class@gmail.com>
2018-06-25 08:49:27 -0700

    First Demo Complete

---
+++ MoviesDemo/MoviesDemo/Controllers/MoviesController.cs
@@ -0,0 +1,38 @@
+﻿using MoviesDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MoviesDemo.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+
+        public MovieRepository repo = new MovieRepository();
+        //Composed of Actions
+        //Action - Method
+
+        //Movies/Display
+        public ActionResult Display()
+        {
+            List<Movie> movies = repo.All();
+            return View("Display", movies);
+        }
+
+
+        protected override void Dispose(bool disposing)
+        {
+            //Cleaning up the repository, in the future, if it needs it.
+            var thing = repo as IDisposable;
+            if (thing != null) thing.Dispose();
+
+            base.Dispose(disposing);
+        }
+
+
+
+    }
+}
---
+++ MoviesDemo/MoviesDemo/Models/ViewModels/Movies/DisplayViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoviesDemo.Models.ViewModels.Movies
+{
+    public class DisplayViewModel
+    {
+        List<Movie> Movies = new List<Movie>();
+        List<Theater> Theaters = new List<Theater>();
+    }
+}
