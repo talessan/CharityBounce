2ed7a6de6e260491948b80003ab3b5341bc516e6
Rob Foulkrod <Azure.Class@gmail.com>
2018-06-26 07:18:10 -0700

    Mid Module 4

---
+++ CharityBounce/CharityBounce/Controllers/NewsletterController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CharityBounce.Controllers
+{
+    public class NewsletterController : Controller //This is normal
+    {
+        [HttpGet]
+        public ViewResult Register() //ViewResult is not completely normal, but better than void
+        {
+            var result = new ViewResult();
+            result.ViewName = "Register";
+            return result;
+        }
+
+        [HttpPost]
+        public void Register(string name, string email)
+        {
+            string message2 = string.Format("{0} has been registered at {1} on {2:d}", name, email, DateTime.Now);
+
+            string message = $"{name} has been registered at {email} on {DateTime.Now:d}"; //String interpolation
+
+            Response.Write(message);
+        }
+    }
+}
---
+++ CharityBounce/CharityBounce/Controllers/NewsletterDemoController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace CharityBounce.Controllers
+{
+    public class NewsletterDemoController : IController //You would not do this normally
+    {
+        private HttpRequestBase Request;
+        private HttpResponseBase Response;
+
+        public void Execute(RequestContext requestContext)
+        {
+            Request = requestContext.HttpContext.Request;
+            Response = requestContext.HttpContext.Response;
+            var actionName = requestContext.RouteData.Values["action"];
+
+            switch (actionName)
+            {
+                case "register":
+                    Register();
+                    break;
+                case "unregister":
+                    Unregister();
+                    break;
+            }
+        }
+
+        private void Unregister()
+        {
+            Response.Write("<h1>You have unsubscribed from the newsletter</h1>");
+
+        }
+
+        private void Register()
+        {
+            Response.Write("<h1>Register Page</h1>");
+            Response.Write("<p>...</p>");
+
+        }
+    }
+}
